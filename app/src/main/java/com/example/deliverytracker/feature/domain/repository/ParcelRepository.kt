5d66d115c254ae05542c00da3caf47be0778c710
추민수 <devsoftychoo@gmail.com>
2024-02-29 22:52:52 +0900

    [Feat] : Repository Pattern

---
+++ app/src/main/java/com/example/deliverytracker/feature/domain/repository/ParcelRepository.kt
@@ -0,0 +1,14 @@
+package com.example.deliverytracker.feature.domain.repository
+
+import com.example.deliverytracker.feature.domain.model.Parcel
+import kotlinx.coroutines.flow.Flow
+
+interface ParcelRepository {
+    fun getParcel(): Flow<List<Parcel>>
+
+    suspend fun getParcelById(id: Int): Parcel?
+
+    suspend fun addParcel(parcel: Parcel)
+
+    suspend fun deleteParcel(parcel: Parcel)
+}
