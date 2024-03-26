db9c3e46ceb6fbb8bd84fd712953143e9b0b321c
추민수 <devsoftychoo@gmail.com>
2024-03-26 22:57:09 +0900

    [Feat] : state

--- app/src/main/java/com/example/deliverytracker/feature/presentation/Parcels/ParcelsViewModel.kt
+++ app/src/main/java/com/example/deliverytracker/feature/presentation/Parcels/ParcelsViewModel.kt
@@ -1,7 +1,11 @@
 package com.example.deliverytracker.feature.presentation.Parcels
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.deliverytracker.feature.domain.usecase.parcel.ParcelUseCases
+import com.example.deliverytracker.feature.domain.util.OrderType
+import com.example.deliverytracker.feature.domain.util.ParcelOrder
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -9,6 +13,8 @@ import javax.inject.Inject
 class ParcelsViewModel @Inject constructor(
     private val parcelUseCases: ParcelUseCases
 ) : ViewModel() {
+    private val _state = mutableStateOf(ParcelState())
+    val state: State<ParcelState> get() = _state
 
 
 }
